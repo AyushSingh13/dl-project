4290ee32f75b238f22acab43f055f3aa2cd40be3
Ayush Singh <ayush.vijay@gmail.com>
2018-01-04 16:22:13 +0000

    Fixed naming error in GradLoss class

--- gatys.py
+++ gatys.py
@@ -20,6 +20,7 @@ def load_image(path):
     img = np.expand_dims(img_to_array(load_img(path, target_size=(256, 256))), axis=0)
     return vgg19.preprocess_input(img)
 
+# Taken from Keras example
 def deprocess_image(x):
     x = x.reshape((img_nrows, img_ncols, 3))
     # Remove zero-center by mean pixel
@@ -57,20 +58,20 @@ assert content_img.shape == style_img.shape == generated_img.shape == (1, 256, 2
 
 input_tensor = K.concatenate([content_img, style_img, generated_img], axis=0)
 
-# Gatys et al. state they do not use any fully-connected layers
+# Gatys et al. specify they do not use any fully-connected layers
 # Average pooling used to give better gradient flow
 model = vgg19.VGG19(input_tensor=input_tensor, include_top=False, pooling='avg')
 
 layers = {layer.name: layer.output for layer in model.layers}
 
-# Match content representation
+# Content representation
 total_loss = K.variable(0.)
 conv4_2_features = layers[content_layer]
 content_img_features = conv4_2_features[0, :, :, :] # 1st element in input_tensor to VGG
 generated_img_features = conv4_2_features[2, :, :, :] # 3rd element in input_tensor to VGG
 total_loss += content_loss(content_img_features, generated_img_features)
 
-# Build style representation
+# Style representation
 style_img_loss = K.variable(0.)
 for name in style_layers:
     layer_features = layers[name]
@@ -89,28 +90,30 @@ else:
 
 generate_func = K.function([generated_img], outputs)
 
-class GradLoss:
+class GradLoss(object):
     def __init__(self):
-        self.loss, self.grads = None, None
+        self._loss, self._grads = None, None
 
-    def _get_loss_and_grads(img):
-        outputs = generated_func([img])
+    def _get_loss_and_grads(self, img):
+        outputs = generate_func([img])
         loss = outputs[0]
         grads = np.array(outputs[1:]).flatten().astype('float64')
         return loss, grads
 
     def loss(self, x):
-        assert self.loss_value is None
+        assert self._loss is None
         loss_value, grad_values = self._get_loss_and_grads(x)
-        self.loss = loss_value
-        self.grads = grad_values
-        return self.loss_value
+        print('LOSS', loss_value)
+        print('GRADS', grad_values)
+        self._loss = loss_value
+        self._grads = grad_values
+        return self._loss
 
     def grads(self, x):
-        assert self.loss_value is not None
-        grad_values = np.copy(self.grad_values)
-        self.loss = None
-        self.grads = None
+        assert self._loss is not None
+        grad_values = np.copy(self._grads)
+        self._loss = None
+        self._grads = None
         return grad_values
 
 grad_and_loss = GradLoss()
@@ -119,7 +122,6 @@ x = load_image('./img/content/nyc.jpg')
 
 for i in range(5):
     print("Iteration %d:" % (i))
-    print(grad_and_loss)
     x, loss_at_min, description = fmin_l_bfgs_b(grad_and_loss.loss,
                                         x, grad_and_loss.grads)
     print("Loss: %d" % (loss_at_min))
